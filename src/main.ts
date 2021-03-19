1532a810ba91467b1f9bccddf0644d0426320c04
Dmitry Baev <baev.dm@gmail.com>
2021-03-19 18:14:14 +0300

    change allure results directory

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function run(): Promise<void> {
     await exec('allurectl job-run start')
     await exec('allurectl job-run plan --output-file', [testPlanJson])
     await exec(
-      'allurectl upload --job-run-child --timeout 1800 target/allure-results & echo "upload started"'
+      'allurectl upload --job-run-child --timeout 1800 build/allure-results & echo "upload started"'
     )
   } catch (error) {
     core.setFailed(error.message)
