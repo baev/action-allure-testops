d235b143586c90fa1a2422fc2a0dcfb0c8e5d4f8
Dmitry Baev <baev.dm@gmail.com>
2021-03-19 18:30:56 +0300

    add logging

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,36 @@
 import * as core from '@actions/core'
 import {exec} from '@actions/exec'
+import {ExecOptions} from '@actions/exec/lib/interfaces'
 
 async function run(): Promise<void> {
   try {
     const testPlanJson = './testplan.json'
-    await exec('allurectl job-run start')
-    await exec('allurectl job-run plan --output-file', [testPlanJson])
+    const execOpts: ExecOptions = {
+      listeners: {
+        debug: data => core.setOutput('debug', data),
+        errline: data => core.setOutput('errline', data),
+        stdline: data => core.setOutput('stdline', data),
+        stderr: data => core.setOutput('stderr', data),
+        stdout: data => core.setOutput('stdout', data)
+      }
+    }
+
+    core.startGroup('allurectl job-run start')
+    await exec('allurectl job-run start', [], execOpts)
+    core.endGroup()
+
+    core.startGroup('allurectl job-run plan')
+    await exec('allurectl job-run plan --output-file', [testPlanJson], execOpts)
+    core.endGroup()
+
+    core.startGroup('allurectl upload')
+
     await exec(
-      'allurectl upload --job-run-child --timeout 1800 build/allure-results & echo "upload started"'
+      'allurectl upload --job-run-child --timeout 1800 build/allure-results & echo "upload started"',
+      [],
+      execOpts
     )
+    core.endGroup()
   } catch (error) {
     core.setFailed(error.message)
   }
--- src/post.ts
+++ src/post.ts
@@ -1,10 +1,26 @@
 import * as core from '@actions/core'
 import {exec} from '@actions/exec'
+import {ExecOptions} from '@actions/exec/lib/interfaces'
 
 async function run(): Promise<void> {
   try {
-    await exec('pkill allurectl')
-    await exec('allurectl job-run stop')
+    const execOpts: ExecOptions = {
+      listeners: {
+        debug: data => core.setOutput('debug', data),
+        errline: data => core.setOutput('errline', data),
+        stdline: data => core.setOutput('stdline', data),
+        stderr: data => core.setOutput('stderr', data),
+        stdout: data => core.setOutput('stdout', data)
+      }
+    }
+
+    core.startGroup('pkill allurectl')
+    await exec('pkill allurectl', [], execOpts)
+    core.endGroup()
+
+    core.startGroup('allurectl job-run stop')
+    await exec('allurectl job-run stop', [], execOpts)
+    core.endGroup()
   } catch (error) {
     core.setFailed(error.message)
   }
